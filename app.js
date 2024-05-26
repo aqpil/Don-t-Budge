a173aabdd881aea670922f25c0dea4980be498bc
Alexandra <pikeleps@gmail.com>
2024-05-26 19:28:25 +0400

    to do: add li id

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-
+//  MAKE NO OPPORTUNITY TO SUBMIT EMPTY INPUTS, MAKE THE PAGE ADAPTABLE
 
 const tick1 = document.querySelector('#tick__form__num2');
-// const cross1 = document.querySelector('#cross__form__num2');
 const necessarySpendInput1 = document.querySelector('#necessarySpendings1');
 const necessarySpendInput2 = document.querySelector('#necessarySpendings2');
 const necessarySpendUL = document.querySelector('#necessarySpendingsUL');
 
+const expenses = [];
+function expense(name, amount) {
+    this.expenseId = `expense${expemseId++}`;
+    this.name = name;
+    this.amount = amount;
+}
 
-const amountsSum = [];
-// let cross1Dupe;
+let expemseId = 0;
+// expense.id = 0;
 
 tick1.addEventListener("click", function (event) {
     event.preventDefault();
     let li = document.createElement('li');
+
 
     let t = document.createTextNode(`${necessarySpendInput1.value} ${necessarySpendInput2.value}`);
     li.appendChild(t);
 
     let cross1Dupe = document.createElement('button');
     cross1Dupe.innerText = '✘';
-    cross1Dupe.addEventListener("click", function () {
+
+    cross1Dupe.addEventListener("click", function (event2) {
+        event2.preventDefault();
+
         li.remove();
     })
 
     li.appendChild(cross1Dupe);
     necessarySpendUL.appendChild(li);
 
-    amountsSum.push(necessarySpendInput2.value);
+    expenses.push(new expense(necessarySpendInput1.value, necessarySpendInput2.value));
     necessarySpendInput1.value = '';
     necessarySpendInput2.value = '';
 });
-
-
 
 
 
@@ -53,7 +60,7 @@ tick2.addEventListener("click", function (event) {
     li.appendChild(cross2Dupe);
     wantToAffordUL.appendChild(li);
 
-    amountsSum.push(wantToAffordInput2.value);
+    expenses.push(wantToAffordInput2.value);
     wantToAffordInput1.value = '';
     wantToAffordInput2.value = '';
 });
@@ -67,7 +74,7 @@ let totalDays = 0;
 function calculateDaysBetween(now, untilDateValue) {
     const untilDate = new Date(untilDateValue);
     let timeDiff = Math.abs(now.getTime() - untilDate.getTime());
-    return Math.ceil(timeDiff / (24 * 60 * 60 * 1000));  // Using Math.ceil to round up to the nearest whole number
+    return Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
 }
 
 // Adding an event listener to the date input to calculate days when a date is selected
@@ -75,7 +82,6 @@ untilDateInput.addEventListener('change', function () {
     let now = new Date();
     let daysBetween = calculateDaysBetween(now, this.value);
     totalDays = daysBetween;
-    // console.log(`Days between now and the selected date: ${daysBetween}`);
 });
 
 
@@ -91,8 +97,27 @@ calculateBtn.addEventListener("click", function (event) {
     const budgetValue = budget.value;
     let sum = 0;
 
-    for (let i = 0; i < amountsSum.length; i++) {
-        sum += (parseInt(amountsSum[i]));
+    for (let i = 0; i < expenses.length; i++) {
+        sum += (parseInt(expenses[i].amount));
     }
     dailyBudget.textContent = (((budgetValue - sum) / totalDays).toFixed(2));
+    console.log(expenses);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS DOESNT WORK! KINDLY REDO :)))
+// let idToRemove = parseInt(li.getAttribute('data-id')); // Get the unique ID from 'li'
+// let removed = expenses.splice(expenses.findIndex(item => item.id === idToRemove), 1); // Remove the corresponding element from the array
+// console.log(removed)
